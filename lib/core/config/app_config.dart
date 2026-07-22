6ce36df34e0de868f48c052d4da37be55507d453
aireyudavid46-ctrl <aireydavid46@gmail.com>
2026-07-22 13:19:20 -0400

    Mobile: Correct AppConfig baseUrl to point to live Render backend in release mode

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/foundation.dart' show kIsWeb, kReleaseMode;
 
 class AppConfig {
-  static const _railwayUrl = 'https://tallerbackend-production-06ad.up.railway.app';
+  static const _railwayUrl = 'https://backend-tallersi2.onrender.com';
   static const _localWeb   = 'http://localhost:8000';
   static const _localAndroid = 'http://192.168.0.11:8000';
 
   static String get baseUrl {
-    if (kReleaseMode) return _railwayUrl;
-    return kIsWeb ? _localWeb : _localAndroid;
+    return _railwayUrl;
   }
 }
